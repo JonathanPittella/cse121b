f9eed47dc390d32d6ecdefebe884f08699b6a7bd
JonathanPittella <jonathanpittella22@gmail.com>
2024-01-17 19:01:28 -0300

    update

--- week 03/templatestring.js
+++ week 03/templatestring.js
@@ -1,1 +1,19 @@
-let output = "Welcome to " + courseSubject + " " + courseNumber + "!Credits: " + courseCredits + "Length: " + courseLength;
+let courseSubject = "Computer Science test";
+let courseNumber = 101;
+let courseCredits = 3;
+let courseLength = "12 weeks test";
+
+let temples = ["Rome", "Salt Lake", "Nauvoo"];
+let listItems = "";
+
+for (const temple of temples) {
+  listItems += `<li>${temple}</li>`;
+}
+
+let output = `Welcome to ${courseSubject} ${courseNumber}! Credits: ${courseCredits} Length: ${courseLength}`;
+
+console.log(output);
+
+document.getElementById("output").textContent += output;
+
+document.querySelector("#templeList").innerHTML = listItems;
